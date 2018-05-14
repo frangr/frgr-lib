b1e0eceb150886bb4420e39d2e568037a85f9a16
fr::gr <frrgrr9@gmail.com>
2018-05-14 23:11:22 +0200

    Add files via upload

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,30 @@
-#include "data/mtvec.h"
-#include "smp/pointy.h"
+#include "mtvec.h"
+#include "pointy.h"
 #include<iostream>
+
+void pf(pointy<int> pi)
+{
+    std::cout<<"pointer: "<<*pi.pointer<<std::endl;
+}
 
 int main()
 {
-    pointy<int> p(false);
+    int* c = new int(500);
+    pointy<int> pft(false, c);
+    pointy<int> pft1(false, new int(7));
+    pointy<int> pft2(true, new int[3]{1, 2, 3} );
+
+    pf(pft);
+    pf(pft1);
+    pf(pft2);
 
     int a = 15;
+
+    pointy<int> pi(false, &a);
+
+    std::cout<<"pi: "<<*pi.pointer<<std::endl;
+
+    pointy<int> p(false);
 
     p.pointer = &a;
 
@@ -17,6 +35,8 @@ int main()
     p2.pointer = new int[3]{1, 2, 3};
 
     std::cout<<"p 0: "<<p2.pointer[0]<<std::endl;
+
+    std::cout<<std::endl<<"---MTVEC---"<<std::endl;
 
     mtvec mv;
 
